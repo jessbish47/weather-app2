5b18e9774e05926cd6d5b3898f076f958c7e8fc3
jessbish47 <101710377+jessbish47@users.noreply.github.com>
2022-03-29 21:37:08 +1000

    border radius removed

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ function displayTemperature(response) {
     let feelsLikeTempElement =document.querySelector("#feels-like");
     let humidityElement =document.querySelector("#humidity");
     let dateElement =document.querySelector("#date");
+
     temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
     conditionElement.innerHTML= response.data.weather[0].description;
     minTempElement.innerHTML= `${Math.round(response.data.main.temp_min)}°`;
@@ -40,7 +41,8 @@ function displayTemperature(response) {
 }
 
 let apiKey="0b0ec56c90b41bb11f010b0e7cfeb75c";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=redbank Plains&appid=${apiKey}&units=metric`;
+let city= "Redbank Plains";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
