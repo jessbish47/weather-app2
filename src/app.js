2cd3ef33cdcdeab84a0008e2a4863d3ced8b506b
jessbish47 <101710377+jessbish47@users.noreply.github.com>
2022-03-29 21:27:55 +1000

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours <10) {
+    hours=`0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10) {
+    minutes =`0${minutes}`;
+}
+let days = [
+    "Sunday",
+    "Monday", 
+    "Tuesday",
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"];
+let day = days[date.getDay()];
+console.log(day);
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response);
     let temperatureElement = document.querySelector("#current-temperature");
@@ -6,18 +29,18 @@ function displayTemperature(response) {
     let maxTempElement =document.querySelector("#max-temperature");
     let feelsLikeTempElement =document.querySelector("#feels-like");
     let humidityElement =document.querySelector("#humidity");
-    temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+    let dateElement =document.querySelector("#date");
+    temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
     conditionElement.innerHTML= response.data.weather[0].description;
     minTempElement.innerHTML= `${Math.round(response.data.main.temp_min)}°`;
     maxTempElement.innerHTML= `${Math.round(response.data.main.temp_max)}°`;
     feelsLikeTempElement.innerHTML= `Feels like: ${Math.round(response.data.main.feels_like)}°`;
-    humidityElement.innerHTML= `Humidity: ${Math.round(response.data.main.humidity)}%`
-    
-
+    humidityElement.innerHTML= `Humidity: ${Math.round(response.data.main.humidity)}%`;
+    dateElement.innerHTML= formatDate(response.data.dt * 1000);
 }
 
 let apiKey="0b0ec56c90b41bb11f010b0e7cfeb75c";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=redbank Plains&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
